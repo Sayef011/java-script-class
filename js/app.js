44d3bc42991359a09bf317f5729ceae26e5d74fe
Sayef011 <155227731+Sayef011@users.noreply.github.com>
2024-11-28 01:26:12 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,102 @@
+let DarkModeBtn = document.querySelector(".navbar .container button.allBtn");
+let body = document.querySelector("body");
+let moonBtn = document.querySelector(".MoonIcon")
+
+function darkModeOnOff() {
+    body.classList.toggle("darkmode") 
+}
+
+DarkModeBtn.addEventListener('click',darkModeOnOff);
+
+let sideBar = document.querySelector(".sidebar");
+let menuBtn = document.querySelector(".navbar .container .menuBtn");
+let CloseBtn = document.querySelector(".sidebar .closeBtn")
+
+function OpenSideBar(){
+sideBar.classList.add("active")
+}
+
+menuBtn.addEventListener('click',OpenSideBar)
+
+function CloseSideBar(event){
+  
+    if(  event.target.classList.contains("sidebar") ||
+   event.target.classList.contains("bi-x-lg") )
+   {
+    sideBar.classList.remove("active")
+   }
+    
+}
+
+CloseBtn.addEventListener('click',CloseSideBar)
+sideBar.addEventListener('click',CloseSideBar)
+
+let mouseMoveFiled = document.querySelector(".mouseMove-test-field")
+let curser = document.querySelector(".curser")
+let curserSm1 = document.querySelector(".curser-sm-1")
+let curserSm2 = document.querySelector(".curser-sm-2")
+let curserSm3 = document.querySelector(".curser-sm-3")
+
+
+function cursorMove(event) {
+  let left = event.pageX;
+  let top = event.pageY;
+  curser.style.top = `${top}px`;
+  curser.style.left = `${left}px`;
+  curserSm1.style.top = `${top}px`;
+  curserSm1.style.left = `${left}px`;
+    curserSm2.style.top = `${top}px`;
+  curserSm2.style.left = `${left}px`;
+    curserSm3.style.top = `${top}px`;
+  curserSm3.style.left = `${left}px`;
+}
+mouseMoveFiled.addEventListener("mousemove", cursorMove);
+
+
+let faqs = document.querySelectorAll(".faq");
+
+function toggleFAQ(event) {
+  for (faq of faqs) {
+    faq.classList.remove("active");
+  }
+
+  event.currentTarget.classList.add("active");
+}
+
+for (faq of faqs) {
+  faq.addEventListener("click", toggleFAQ);
+}
+
+
+
+$(window).scroll(function(){
+  let scrollTop = $(window).scrollTop();
+  if(scrollTop >= 300){
+    $('nav').addClass('fixed')
+  }if(scrollTop <= 300){
+    $('nav').addClass('RemoveFixed')
+  }
+  else{
+    $('nav').removeClass('RemoveFixed')
+  }
+})
+
+
+
+$(window).scroll(function(){
+  let scrollTop = $(window).scrollTop();
+  if(scrollTop > 400){
+    $(".BackToTop").show(500)
+  }else{
+    $(".BackToTop").hide(500)
+  }
+})
+
+$(".BackToTop").click(function(){
+  $('html,body').animate({scrollTop: 0}, 500)
+})
+
+$(window).on("load",function(){
+$(".preloader").fadeOut(500)
+})
+
